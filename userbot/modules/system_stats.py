d81c483d807d57dcdf433f7c519ebab5e354f73d
shaquille03 <84997691+shaquille03@users.noreply.github.com>
2021-06-01 08:59:06 +0800

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -319,22 +319,22 @@ async def redis(alive):
     await alive.edit("⚫𝐇𝐢𝐫𝐨-𝐔𝐬𝐞𝐫𝐛𝐨𝐭​⚫")
     await asyncio.sleep(1)
     await alive.edit("🔥")
-    await asyncio.sleep(3)
+    await asyncio.sleep(2)
     await alive.edit("💀")
-    await asyncio.sleep(3)
+    await asyncio.sleep(2)
     output = (
         f"**╔══════━━━━━━━══════╗ ** \n"
         f"**      ⚫𝐇𝐢𝐫𝐨-𝐔𝐬𝐞𝐫𝐛𝐨𝐭​⚫       ** \n"
         f"**╚══════━━━━━━━══════╝ ** \n"
-        f"  ╔══════━━━━━━━═══════╗\n"
-        f"  ╟◈  `User     :`{DEFAULTUSER} \n"
-        f"  ╟◈  `Username :`@{user.username} \n"
-        f"  ╟◈  `Telethon :`v. {version.__version__} \n"
-        f"  ╟◈  `Python   :`v. {python_version()} \n"
-        f"  ╟◈  `Branch   :`{UPSTREAM_REPO_BRANCH} \n"
-        f"  ╟◈  `Bot Ver  :`v. {BOT_VER} \n"
-        f"  ╟◈  `Plugins  :`{len(modules)} Plugins \n"
-        f"  ╚══════━━━━━━━═══════╝ \n"
+        f"╔══════━━━━━━━═══════╗\n"
+        f"╟◈ `User     :`{DEFAULTUSER} \n"
+        f"╟◈ `Username :`@{user.username} \n"
+        f"╟◈ `Telethon :`v. {version.__version__} \n"
+        f"╟◈ `Python   :`v. {python_version()} \n"
+        f"╟◈ `Branch   :`{UPSTREAM_REPO_BRANCH} \n"
+        f"╟◈ `Bot Ver  :`v. {BOT_VER} \n"
+        f"╟◈ `Plugins  :`{len(modules)} Plugins \n"
+        f"╚══════━━━━━━━═══════╝ \n"
         f"| [Repo](https://apisuserbot.github.io/King-Userbot) | [King-Team](t.me/KingUserbotSupport) | "
         f"[Pemilik](t.me/iamhiro)")
     if ALIVE_LOGO:
