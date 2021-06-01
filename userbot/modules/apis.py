02f3a4582bf9bf738b8e26205a5cdd455f8c2eae
shaquille03 <84997691+shaquille03@users.noreply.github.com>
2021-06-01 22:49:25 +0800

    Update apis.py

--- userbot/modules/apis.py
+++ userbot/modules/apis.py
@@ -4,6 +4,26 @@ from userbot.events import register
 from telethon import events
 import asyncio
 
+@register(outgoing=True, pattern="^.amb$")
+async def koc(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("AMBULANCE")
+        sleep(1)
+        await e.edit("HARAP")        
+        await e.edit("MINGGIR")        
+        await e.edit("🔵🔵⬜⬜🔴🔴
+🔵🔵⬜⬜🔴🔴")        
+        await e.edit("🔴🔴⬜⬜🔵🔵
+🔴🔴⬜⬜🔵🔵")        
+        await e.edit("🔵🔵⬜⬜🔴🔴
+🔵🔵⬜⬜🔴🔴")       
+        await e.edit("🔴🔴⬜⬜🔵🔵
+🔴🔴⬜⬜🔵🔵")                        
+        await e.edit("🔵🔵⬜⬜🔴🔴
+🔵🔵⬜⬜🔴🔴")       
+        await e.edit("🔴🔴⬜⬜🔵🔵
+🔴🔴⬜⬜🔵🔵")       
+
 
 @register(outgoing=True, pattern="^.kak$")
 async def koc(e):
